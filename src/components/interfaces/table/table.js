d1516e2766a4719534466868a81d62c0fbf2597b
kolbeinsson <steinthor.kolbeinsson@bouvet.no>
2019-12-04 15:45:03 +0100

    Navbar update

--- src/components/interfaces/table/table.js
+++ src/components/interfaces/table/table.js
@@ -3,28 +3,23 @@ import styled from 'styled-components';
 
 const StyledTable = styled.table`
     padding: 10px;
-
     thead {
         appearance: none;
         border: 1px solid green;
     }
-
     thead {
         padding: 10px;
     }
-
     .body {
         margin: 40px;
         border: 1px solid black;
     }
-
     .head {
         background-color: black;
         color: white;
         font-weight: 500;
         border: none;
     }
-
     td {
         min-width: 100px;
     }
