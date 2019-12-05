863f6a004e11b6b84f6dc7d79fe6db146e2aef2f
kolbeinsson <steinthor.kolbeinsson@bouvet.no>
2019-12-05 10:00:22 +0100

    Customize image height and width

--- src/components/interfaces/carousel/Carousel.js
+++ src/components/interfaces/carousel/Carousel.js
@@ -4,12 +4,11 @@ import styled from 'styled-components';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 
 const StyledCarousel = styled.div`
-    border: 1px solid black;
     background-image: url(${props => props.url});
     background-origin: center;
     background-position-y: bottom;
-    width: 400px;
-    height: 400px;
+    width: ${props => props.width}px;
+    height: ${props => props.height}px;
     position: relative;
 
     span {
@@ -50,15 +49,15 @@ class Carousel extends React.Component {
 
     render () {
         const { index } = this.state;
-        const { data } = this.props;
+        const { img, height, width } = this.props;
 
         let currentIndex = index;
 
-        if (currentIndex === -1) { currentIndex = data.length-1; }
-        else if (currentIndex === data.length) { currentIndex = 0; };
+        if (currentIndex === -1) { currentIndex = img.length-1; }
+        else if (currentIndex === img.length) { currentIndex = 0; };
         
         return (
-            <StyledCarousel key={data[currentIndex]} url={data[currentIndex]}>
+            <StyledCarousel key={img[currentIndex]} url={img[currentIndex]} height={height}  width={width}>
                 <span onClick={() => this.setState({ index: --currentIndex })}><FaAngleLeft size="30px" color="grey" /></span>
                 <span onClick={() => this.setState({ index: ++currentIndex })}><FaAngleRight size="30px" color="grey" /></span>
             </StyledCarousel>
